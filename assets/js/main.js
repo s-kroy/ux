257761ccf2ae3786e75bcdb94670ae7ab4c41feb
Suman Kanti Roy <roy.sumankanti@gmail.com>
2021-06-11 20:56:44 +0530

    vg

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,14 +165,6 @@
     }
   });
 
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
   // Initi AOS
   AOS.init({
     duration: 1000,
